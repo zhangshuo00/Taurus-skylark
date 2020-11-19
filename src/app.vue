00018553916b21da14d24b3957e72b1993ada4ed
zhangshuo <zhangshuo1@bonc.com.cn>
2020-11-19 17:39:15 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name:'app',
+  data(){
+    return {
+      msg: 'hello skylark'
+    }
+  }
+}
+</script>
