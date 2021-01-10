2e91724be41d3782a87cfccaadf5dac0adff81ac
zhangshuo <zhangshuo1@bonc.com.cn>
2021-01-10 16:54:17 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Taurus-skylark
+# 云雀 | 知识共享平台
+
+基于 vue3.0 + vite + typescript 的测试版本
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import './styles/main.css'
-import App from './app.vue'
-
-new Vue({
-    el: '#app',
-    template: '<App/>',
-    components: { App }
-})
