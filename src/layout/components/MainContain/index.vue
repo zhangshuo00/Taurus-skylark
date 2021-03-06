3c8adcf9078f24dbbd38335142acef3fd529b571
zhangshuo <zhangshuo1@bonc.com.cn>
2021-03-06 11:55:08 +0800

    nav|side|main|layout

---
+++ src/layout/components/MainContain/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="main-contain">
+<!--    <router-view :key="key" />-->
+  main-contain
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MainContain',
+  computed: {
+    key() {
+      // return this.$route.path
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-contain {
+  min-height: calc(100vh - 80px);
+  width: 100%;
+  background: #DCDFE6;
+}
+</style>
