c1e909ce35054566eb732ccd33c0f4b471e42484
zhangshuo <zhangshuo1@bonc.com.cn>
2021-03-19 09:53:13 +0800

    修改响应式布局

---
+++ src/views/subscriptions/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="subscript-index">
+    关注的全部的话题
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Subscriptions'
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/user/settings/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="setting-"></div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
